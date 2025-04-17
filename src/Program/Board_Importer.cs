72a77ea386dbaf7e5dbc81e321d127667e679d50
Agustin Pechi <AGUSTIN.PECHI@correo.ucu.edu.uy>
2025-04-16 23:55:52 -0300

    Comentarios agregados a board_importer

--- src/Program/Board_Importer.cs
+++ src/Program/Board_Importer.cs
@@ -5,17 +5,17 @@ namespace Program
     using System.IO;
     public class BoardImporter
     {
-        public static bool[,] ImportFromFile(string url)
+        public static bool[,] ImportFromFile(string url)//url es la direccion del archivo .txt
         {
             string[] content = File.ReadAllText(url).Split('\n');
-            bool[,] board = new bool[content.Length, content[0].Length];
+            bool[,] board = new bool[content.Length, content[0].Length];//Creo una matriz de las dimensiones del archivo .txt con valores booleanos en false por defecto
             for (int i = 0; i < content.Length; i++)
             {
                 for (int j = 0; j < content[0].Length; j++)
                 {
                     if (content[i][j] == '1')
                     {
-                        board[i, j] = true;
+                        board[i, j] = true;//Se vuelve true cuando la celula esta viva, es decir, cuando en el archivo es 1
                     }
                 }
             }
